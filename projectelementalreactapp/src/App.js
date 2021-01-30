0e22561de191d8b1ff7128f0b624ab9bfd0f0a32
Ricky Felix <rickyfelix19@outlook.com>
2021-01-31 01:34:15 +1100

    Make Routing Skeleton

--- projectelementalreactapp/src/App.js
+++ projectelementalreactapp/src/App.js
@@ -1,9 +1,41 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom" // this comes from install npm react-router-dom
+
+class App extends React.Component { 
+  render() {
+    return ( // website nav pane
+      <div> 
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+            <ul>
+              <li>Kabinet Sinergi 2020-2021</li>
+              <li>Vision Mission 2020-2021</li>
+              <li>Contact</li>
+              <li>AD/ART</li>
+            </ul>
+          <li>Chapters</li>
+            <ul>
+              <li>New South Wales - ISA NSW</li>
+              <li>Northern Territory - PPIANT</li>
+              <li>Queensland - PPIA Queensland</li>
+              <li>South Australia</li>
+              <li>Tasmania - PPIATasmania</li>
+              <li>Victoria - PPIAVIC</li>
+              <li>Western Australia</li>
+            </ul>
+          <li>Live in OZ</li>
+          <li>News</li>
+            <ul>
+              <li>Events</li>
+              <li>Upcoming</li>
+            </ul>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
