ec0073cf6116af9b195a79c5bedb47392be777ec
Ricky Felix <rickyfelix19@outlook.com>
2021-01-31 18:44:38 +1100

    Test for Slack Notification

--- projectelementalreactapp/src/pages/home/Home.jsx
+++ projectelementalreactapp/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ class Home extends React.Component {
         return (
             <div>
                 <h1>Successful Home</h1>
+                <h1>test</h1>
             </div>
         );
     }
