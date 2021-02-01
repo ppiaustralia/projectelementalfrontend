dc5d8ce71a7b70240fa6b4f8e7e30f5a7b446fba
Daniel Handiar <dhandiar@student.unimelb.edu.au>
2021-02-01 22:59:11 +0700

    Introduced Carousel for Homepage
    * Created Carousel component for homepage
    * Added dummy images for carousel
    * Added bootstrap in index.js

--- projectelementalreactapp/src/pages/home/Home.jsx
+++ projectelementalreactapp/src/pages/home/Home.jsx
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import ReactDOM from 'react-dom';
 
-class Home extends React.Component {
+import "./Home.css";
+
+import dummy1 from "../../assets/images/cat.jpg";
+import dummy2 from "../../assets/images/cat2.jpg";
+import dummy3 from "../../assets/images/cat3.jpg";
+
+export default class Home extends React.Component {
     render() {
-        return (
+        return(
             <div>
-                <h1>Successful Home</h1>
-                <h1>test</h1>
+                <ImageSlider />
+                <h3>Chapters</h3>
+                <h3>Latest Update</h3>
             </div>
         );
     }
 }
 
-export default Home;
+export class ImageSlider extends Component {
+    render() {
+        return (
+                <div className = "background">
+                    <Carousel>
+                        {/*
+                        image needs a class="d-block mx-auto img-fluid w-1439" to fit within
+                        the carousel 
+                        */}
+                        <Carousel.Item>
+                            <img class="d-block mx-auto img-fluid w-1439" src={dummy1}/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img class="d-block mx-auto img-fluid w-1439" src={dummy2}/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img class="d-block mx-auto img-fluid w-1439" src={dummy3}/>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+        );
+    }
+}
