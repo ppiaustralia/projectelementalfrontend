e3fc65aca17e8b67850e4b857c7b87411451d179
Heriyanto <buibui_chen@yahoo.com>
2021-02-12 14:30:03 +1100

    Unfinished About Us page

--- projectelementalreactapp/src/pages/about/About.jsx
+++ projectelementalreactapp/src/pages/about/About.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Sidebar from './Sidebar';
 
 class About extends React.Component {
     render() {
         return (
             <div>
+            <Sidebar/>
                 <h1>Successfully route to About page</h1>
             </div>
         );
