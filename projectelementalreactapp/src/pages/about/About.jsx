21a25c1677607f3b288ec0d20ae1c23f91a61e6d
Heriyanto <buibui_chen@yahoo.com>
2021-02-26 10:26:55 +1100

    Mission and Vision

--- projectelementalreactapp/src/pages/about/About.jsx
+++ projectelementalreactapp/src/pages/about/About.jsx
@@ -6,12 +6,15 @@ import "./Pdfreader.css";
 
 import samplePDF from './sample.pdf'; /* This is required only if the project file is located 
 inside the app. Otherwise you can use the external link of the pdf file*/
+import MissionVision from './MissionVision';
 
 class About extends React.Component {
     render() {
         return (
             <div>
-            <Sidebar/>
+                <MissionVision/>
+           {/* <Sidebar/> */}
+           <h4>AD / ART</h4>
             <SinglePagePDFViewer pdf={samplePDF}/>
             </div>    
         );
---
+++ projectelementalreactapp/src/pages/about/MissionVision.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+  margin: 10rem auto 0 auto;
+  width: 60%;
+
+  .heading--main {
+    text-align: left
+    ;
+  }
+`;
+
+const MissionVision = () => {
+    return (
+      <StyledSection className="About Us">
+        <div className="block_text">
+          <div className="aboutUs_info">
+            <h1 className="heading--main">About Us</h1>
+            <p>
+            Perhimpunan Pelajar Indonesia Australia (PPIA) or Indonesian Students’ Association of Australia has been established on March 13, 1981 in Canberra, Australian Capital Territory.
+            As one of the most prominent Indonesian student organisation in Australia, it has purpose to serve and unite Indonesian students across Australia. 
+            Moreover, PPIA is a medium of communication and interaction for all Indonesian students across Australia. 
+            The goals are to establish a strong sense of fellowship among Indonesian students and gives contribution to the nation in terms of cultural recognition as a foreign community.
+            PPIA comprises of 8 state-level organisations (PPIA Chapters) and 33 university-level organisations (PPIA Branches). 
+            PPIA represents the interests and caters for the needs of all Indonesia students.
+            For the success of its activities, PPIA is affiliated with the Embassy of the Republic of Indonesia and the Consulate General of the Republic of Indonesia along with the cooperation of the Indonesian community. 
+            Thus, PPIA becomes an organisation that has a large exposure to the wider community in Australia, be it Indonesians, locals and other international communities.
+            </p>
+          </div>
+        </div>
+
+        <div className="block_text">
+          <div className="vision_info">
+            <h1 className="heading--main">Vision</h1>
+            <p>
+            Mewujudkan Perhimpunan Pelajar Indonesia-Australia (PPIA) sebagai organisasi yang berkontribusi membangun generasi muda Indonesia yang peduli, mandiri, kreatif dan inovatif untuk kemajuan bangsa dan negara.
+            </p>
+          </div>
+        </div>
+
+        <div className="block_text">
+          <div className="mission_info">
+            <h1 className="heading--main">Mission</h1>
+            <p>
+            Membangun rasa kepedulian dan kontribusi positif kepada masyarakat Indonesia di Australia terutama para mahasiswa.
+
+            Membangun platform berasas kekeluargaan yang memberikan kesempatan bagi mahasiswa-mahasiswa Indonesia untuk memperluas koneksi mereka dari berbagai kota Australia.
+
+            Mengembangkan sistem organisasi yang efisien dan peduli.
+            </p>
+          </div>
+        </div>
+      </StyledSection>
+    );
+  };
+  
+  export default MissionVision;
