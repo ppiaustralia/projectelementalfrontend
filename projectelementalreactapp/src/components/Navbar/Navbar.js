00e84fdc067b8069a5e15143ba4300b35f6e33ff
Heriyanto <buibui_chen@yahoo.com>
2021-02-26 22:50:46 +1100

    Chapter dropdown

--- projectelementalreactapp/src/components/Navbar/Navbar.js
+++ projectelementalreactapp/src/components/Navbar/Navbar.js
@@ -1,89 +1,85 @@
-import React, { useState} from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import Dropdown from './Dropdown';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Dropdown from "./Dropdown";
 
-function Navbar () {
+function Navbar() {
+  const [click, setclick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-    const [click, setclick] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
+  const handleClick = () => setclick(!click);
+  const closeMobileMenu = () => setclick(false);
 
-    const handleClick = () => setclick(!click);
-    const closeMobileMenu = () => setclick(false);
-
-    const onMouseEnter = () => {
-        if(window.innerWidth < 960) {
-            setDropdown(false)
-        } else {
-            setDropdown(true)
-        }
-    };
-
-    const onMouseLeave = () => {
-        if(window.innerWidth < 960) {
-            setDropdown(false)
-        } else {
-            setDropdown(false)
-        }
-    };
-
-
-    return (
-        <>
-       <nav className='navbar'>   
-        <Link to='/' className='navbar-logo'>
-            PPIA
-        </Link>
-        <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                    Home
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
-                    About Us
-                </Link>
-            </li>
-            <li className='nav-item'
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            
-            >
-                {/* changed from "Link" to "p" to prevent redirect to /chapter/ */}
-                <li className='nav-links' onClick={closeMobileMenu}>
-                    Chapters <i className='fas fa-caret-down'/>
-                </li>
-                {dropdown && <Dropdown />}
-            </li>
-            <li className='nav-item'>
-                <Link to='/liveinoz' className='nav-links' onClick={closeMobileMenu}>
-                    Live in OZ
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/blog' className='nav-links' onClick={closeMobileMenu}>
-                    Blog
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
-                    Contact
-                </Link>
-            </li>
-        </ul>
-       </nav>
-       </> 
-    )
-
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
     }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          PPIA
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+              About Us
+            </Link>
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            {/* changed from "Link" to "p" to prevent redirect to /chapter/ */}
+            <li className="nav-links" onClick={closeMobileMenu}>
+              Chapters <i className="fas fa-caret-down" />
+            </li>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/liveinoz"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Live in OZ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/blog" className="nav-links" onClick={closeMobileMenu}>
+              Blog
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
 
 export default Navbar;
-
-
-
-
-   
