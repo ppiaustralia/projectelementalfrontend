02fd6c12b38a455d4bef76b9998ff654e6882407
Daniel Handiar <dhandiar@student.unimelb.edu.au>
2021-03-04 17:07:15 +0700

    Started Live in Oz
    * Started the Code for Live in Oz
    * [had to npm audit fix]

--- projectelementalreactapp/src/pages/liveinoz/Liveinoz.jsx
+++ projectelementalreactapp/src/pages/liveinoz/Liveinoz.jsx
@@ -1,11 +1,49 @@
 import React, {Component} from 'react';
+import {Image} from 'react-bootstrap/';
 import ReactDOM from 'react-dom';
+
+import './Liveinoz.css';
+
+
+function titlePiece(textInput) {
+    return (
+    <div>
+        <h3>{textInput}</h3>
+        <hr style={{width: '70%', margin: 'auto'}} />
+    </div>
+    );
+}
+
+function entryPiece(dataInput) {
+    return (
+        <div>
+            {dataInput}
+        </div>
+    );
+}
 
 class Liveinoz extends React.Component {
     render() {
         return (
+            <div className = "page">
+                <div>
+                    <h1>Student Guide</h1>
+                </div>
+                <hr style={{width: '95%', margin: 'auto'}} />
+            </div>
+        );
+    }
+}
+
+class Entry extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
             <div>
-                <h1>Successful LiveinOZ</h1>
+                <h3>Topic page</h3>
             </div>
         );
     }
