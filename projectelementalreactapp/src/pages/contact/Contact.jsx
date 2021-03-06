23979f21a2260d469fbe27021c1fa54eecc9d07e
Ricky Felix <rickyfelix19@outlook.com>
2021-03-06 22:32:36 +1100

    notes for Contact

--- projectelementalreactapp/src/pages/contact/Contact.jsx
+++ projectelementalreactapp/src/pages/contact/Contact.jsx
@@ -1,14 +1,30 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-
-import ContactDatabase from 'ContactDatabase.js'
-import 'Contact.css'
+import React from 'react';
+import './Contact.css'
+import ContactDatabase from './ContactDatabase'
 
 function Contact(props) {
     
-    return(
-        <p>something</p>
-    )
+    return (
+        <div>
+            <h1>Contact</h1> {/* title */}
+            <div>
+                {/* call PPIAustralia */}
+                
+            </div>
+            <div>
+                <h3>The information below are the lists of our embassy and consulates that are located in Australia</h3>
+                <div>
+                    <h1>Embassy</h1> {/* title */}
+                    <h3>the lists of consulate general in Australia</h3> 
+
+                    {/* call Embassy */}
+                    
+                    {/* call everything with Consulate */}
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
--- projectelementalreactapp/src/pages/contact/ContactDatabase.js
+++ projectelementalreactapp/src/pages/contact/ContactDatabase.js
@@ -1,9 +1,16 @@
 const ContactDatabase = {
 
+    PPIAustralia : [
+        {
+            name        : "PPI Australia",
+            contact     : ["president@ppi-australia.org", "publicrelations@ppi-australia.org"]
+        }
+    ],
+
     Embassy_ACT : [
         {
             name        : "THE EMBASSY OF THE REPUBLIC OF INDONESIA IN CANBERRA, AUSTRALIA",
-            address    : "8, Darwin Ave, Yarralumla ACT 2600",
+            address     : "8, Darwin Ave, Yarralumla ACT 2600",
             number      : "(02) 6250 8600",
             website     : "https://kemlu.go.id/canberra/en"
         }
@@ -12,7 +19,7 @@ const ContactDatabase = {
     Consulate_NT : [
         {
             name        : "Consulate General of The Republic of Indonesia for DARWIN, NORTHERN TERRITORY, AUSTRALIA",
-            address    : "20 Harry Chan Avenue, Darwin NT 0800",
+            address     : "20 Harry Chan Avenue, Darwin NT 0800",
             number      : "(08) 8943 0200",
             website     : "https://kemlu.go.id/darwin/en"
         }
@@ -21,7 +28,7 @@ const ContactDatabase = {
     Consulate_NSW_QLD_SA : [
         {
             name        : "Consulate General of The Republic of Indonesia for NEW SOUTH WALES, QUEENSLAND, AND SOUTH AUSTRALIA",
-            address    : "BASED IN SYDNEY, AUSTRALIA 236-238 Maroubra Rd, Maroubra NSW 2035",
+            address     : "BASED IN SYDNEY, AUSTRALIA 236-238 Maroubra Rd, Maroubra NSW 2035",
             number      : "(02) 9314 0872",
             website     : "https://kemlu.go.id/sydney/en"
         }
@@ -30,7 +37,7 @@ const ContactDatabase = {
     Consulate_VIC : [
         {
             name        : "Consulate General of The Republic of Indonesia for MELBOURNE-VICTORIA, AUSTRALIA",
-            address    : "72 Queens Rd, Melbourne VIC 3004",
+            address     : "72 Queens Rd, Melbourne VIC 3004",
             number      : "(03) 9525 2755",
             website     : "https://kemlu.go.id/melbourne/en"
         }
@@ -39,7 +46,7 @@ const ContactDatabase = {
     Consulate_WA : [
         {
             name        : "Consulate General of The Republic of Indonesia for PERTH, AUSTRALIA",
-            address    : "134 Adelaide Terrace, East Perth WA 6004",
+            address     : "134 Adelaide Terrace, East Perth WA 6004",
             number      : "(08) 9221 5858",
             website     : "https://kemlu.go.id/perth/en"
         }
