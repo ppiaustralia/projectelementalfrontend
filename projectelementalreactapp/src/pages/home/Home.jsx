77c614af70923fb25bd83d5be5247550623283a0
Daniel Handiar <dhandiar@student.unimelb.edu.au>
2021-04-01 20:35:30 +0700

    Updates to Homepage
    * Added links to homepage
    - Chapters redirection
    * Minor Ux changes

--- projectelementalreactapp/src/pages/home/Home.jsx
+++ projectelementalreactapp/src/pages/home/Home.jsx
@@ -1,74 +1,11 @@
 import React, {Component} from 'react';
-import {Carousel, Container, Row, Col, Image, CardGroup, Card, Button} from 'react-bootstrap/';
+import {Link} from "react-router-dom";
+import {Carousel, Container, Row, Col, Image, Card, Button, CardDeck} from 'react-bootstrap/';
 import ReactDOM from 'react-dom';
 
+import {image_arr, source_arr, update_arr} from './database'
+
 import "./Home.css";
-
-import dummy1 from "../../assets/images/cat.jpg";
-import dummy2 from "../../assets/images/cat2.jpg";
-import dummy3 from "../../assets/images/cat3.jpg";
-
-const image_arr = [
-    <img class="d-block mx-auto responsive-carousel img-fluid" src={dummy1}/>,
-    <img class="d-block mx-auto responsive-carousel img-fluid" src={dummy2}/>,
-    <img class="d-block mx-auto responsive-carousel img-fluid" src={dummy3}/>
-];
-
-const source_arr = [
-    {
-        img: dummy1,
-        name: "cat1"
-    },
-    {
-        img: dummy2,
-        name: "cat2"
-    },
-    {
-        img: dummy3,
-        name: "cat3"
-    },
-    {
-        img: dummy1,
-        name: "cat1"
-    },
-    {
-        img: dummy2,
-        name: "cat2"
-    },
-    {
-        img: dummy3,
-        name: "cat3"
-    },
-    {
-        img: dummy1,
-        name: "cat1"
-    },
-    {
-        img: dummy2,
-        name: "cat2"
-    }
-];
-
-const latest_updates =[
-    {
-        img: dummy1,
-        title: "Cat picture 1",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-        updated: 3
-    },
-    {
-        img: dummy2,
-        title: "Cat picture 2",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-        updated: 5
-    },
-    {
-        img: dummy3,
-        title: "Cat picture 3",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-        updated: 4
-    }
-]
 
 export default class Home extends React.Component {
     render() {
@@ -110,8 +47,10 @@ export class ImageGallery extends Component {
                         <Row className="justify-content-md-center">
                                 {source_arr.map((chapter,i) => 
                                     <Col md="auto">
-                                    <Image src={chapter.img} className='d-block responsive-gallery img-fluid mx-4' roundedCircle/>
-                                    <h5>{chapter.name}</h5>
+                                        <Link to={chapter.path}>
+                                            <Image src={chapter.img} className='d-block responsive-gallery img-fluid mx-4' roundedCircle/>
+                                        </Link>
+                                        <h5>{chapter.title}</h5>
                                     </Col>
                                 )}
                         </Row>
@@ -130,26 +69,32 @@ export class NewsCards extends Component {
                     <h3>Latest Updates</h3>
                 </div>
                 <div className = 'news-cards'>
-                    <CardGroup>
-                        {latest_updates.map((news,i) => 
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant = 'top' src = {news.img}/>
-                                <Card.Body>
-                                    <Card.Title>
-                                        {news.title}
-                                    </Card.Title>
-                                    <Card.Text>
-                                        {news.desc}
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className='text-muted'>Last updated {news.updated} mins ago</small>
-                                </Card.Footer>
-                            </Card>
+                    <CardDeck style={{display: 'flex', flexDirection: 'row'}}>
+                        {update_arr.map((news,i) => 
+                                <div>
+                                <Card style={{ width: '18rem', flex: 1, maxWidth: '90%'}}>
+                                    <Link to={news.path}>
+                                        <Card.Img variant = 'top' src = {news.img} className = "card-img"/>
+                                    </Link>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            {news.title}
+                                        </Card.Title>
+                                        <Card.Subtitle>
+                                            {news.date}
+                                        </Card.Subtitle>
+                                        <Card.Text>
+                                            {news.desc}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                </div>
                         )}
-                    </CardGroup>
+                    </CardDeck>
                 </div>
-                <Button variant = "news" >Read More</Button>
+                <Link to="/blog">
+                    <Button variant = "news" style = {{width: "116px"}} >Read More</Button>
+                </Link>
             </div>
         );
     }
