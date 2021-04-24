1b1c5db5f05a312c42fc548368d7e8230c73410b
Daniel Handiar <dhandiar@student.unimelb.edu.au>
2021-04-24 23:03:35 +0700

    Live in Oz
    Created share icons

--- projectelementalreactapp/src/pages/liveinoz/database.jsx
+++ projectelementalreactapp/src/pages/liveinoz/database.jsx
@@ -1,47 +1,34 @@
-export const max = 2;
 export const data_arr = [
+    // {
+    //     type: 'subtitle',
+    //     heading: 'Menjelang Keberangkatan ke Australia'
+    // },
     {
+        type: 'entry',
         img: '/assets/images/PPIA.jpg',
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
     },
     {
+        type: 'entry',
+        img: '/assets/images/PPIA.jpg',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
+    },
+    // {
+    //     type: 'subtitle',
+    //     heading: 'Tiba di Australia'
+    // },
+    {
+        type: 'entry',
+        img: '/assets/images/PPIA.jpg',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
+    },
+    {
+        type: 'entry',
         img: '/assets/images/PPIA.jpg',
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
     }
 ]
-
-
-// Ignore for now
-//
-// function titlePiece(textInput) {
-//     return (
-//     <div>
-//         <h3>{textInput}</h3>
-//         <hr style={{width: '70%', margin: 'auto'}} />
-//     </div>
-//     );
-// }
-//
-// function entryPiece(dataInput) {
-//     return (
-//         <div>
-//             {dataInput}
-//         </div>
-//     );
-// }
-//
-// class Entry extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//
-//     render() {
-//         return (
-//             <div>
-//                 <h3>Topic page</h3>
-//             </div>
-//         );
-//     }
-// }
