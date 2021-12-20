7a25ce4aee346cadf6d12c88633293a9a6d18306
Ricky Felix <rickyfelix19@outlook.com>
2021-12-20 12:01:59 +1100

    add files from Richard Forks

--- projectelementalreactapp/src/pages/contact/Contact.jsx
+++ projectelementalreactapp/src/pages/contact/Contact.jsx
@@ -1,37 +1,48 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import "./Contact.css"
-import ContactDatabase from "./ContactDatabase.json"
-import Embassy from "./Embassy"
-import ConsulatesCard from "./ConsulatesCard"
-import PPIA from "./PPIA"
+import './Contact.css';
+import ContactDatabase from './ContactDatabase.json';
+import Embassy from './Embassy';
+import ConsulatesCard from './ConsulatesCard';
+import PPIA from './PPIA';
 
-function Contact() {
-    const [contact, setContact] = useState()
-    const contacts = ContactDatabase
-    const baseURL = "https://ppiacontact.s3.us-east-2.amazonaws.com/"
+// import { Container, Row, Col, Image } from 'react-bootstrap/';
 
-    return (
-        <div className="container mt-3">
-            <div>
-                <PPIA data={contacts.PPIAustralia} />
-            </div>
-            <div>
-                <h3>
-                    The information below are the lists of our embassy and
-                    consulates that are located in Australia
-                </h3>
-                <div>
-                    <Embassy data={contacts.Embassy} />
-                </div>
-                <div>
-                    {contacts.Consulate.map((element) => {
-                        return <ConsulatesCard data={element} />
-                    })}
-                </div>
-            </div>
+function Contact(data) {
+  const [contact, setContact] = useState([]);
+  const contactImage = 'https://ppiacontact.s3.us-east-2.amazonaws.com/';
+
+  useEffect(() => {
+    setContact([]);
+
+    axios
+      .get('https://ppia-backend.herokuapp.com/external_contact/')
+      .then((props) => {
+        setContact(data.data);
+        //console.log(data.data)
+      });
+  });
+
+  return (
+    <div className="container mt-3">
+      <div>
+        <PPIA data={contact.PPIAustralia} />
+      </div>
+      <div>
+        <p>
+          The information below are the lists of our embassy and consulates that
+          are located in Australia
+        </p>
+        <div>
+          <Embassy data={contact.Embassy} />
         </div>
-    )
+        <div>
+          <ConsulatesCard data={contact.ConsulatesCard} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
