434999957f88e2b61bdfc4694d101b612ab40f67
Ricky Felix <rickyfelix19@outlook.com>
2021-12-23 14:28:54 +1100

    add 404 page

--- projectelementalreactapp/src/App.js
+++ projectelementalreactapp/src/App.js
@@ -1,52 +1,53 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { withRouter } from "react-router"
-import Home from "../src/pages/home/Home"
-import Navbar from "./layout/navbar/Navbar"
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import Home from '../src/pages/home/Home';
+import Navbar from './layout/navbar/Navbar';
 
-import Chapter from "../src/pages/chapter/Chapter"
-import About from "../src/pages/about/About"
-import Contact from "../src/pages/contact/Contact"
-import Liveinoz, { ReadMore } from "../src/pages/liveinoz/Liveinoz"
-import Blog from "../src/pages/blog/Blog"
-import Scholarship from "../src/pages/scholarship/Scholarship"
+import Chapter from '../src/pages/chapter/Chapter';
+import About from '../src/pages/about/About';
+import Contact from '../src/pages/contact/Contact';
+import Liveinoz, { ReadMore } from '../src/pages/liveinoz/Liveinoz';
+import Blog from '../src/pages/blog/Blog';
+import Scholarship from '../src/pages/scholarship/Scholarship';
 // import { FooterContainer } from "./layout/footer/footer";
-import Footer from "./layout/newFooter/Footer"
+import NotFound404 from '../src/pages/404/PageUnavailable.jsx';
+import Footer from './layout/newFooter/Footer';
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" exact component={About} />
-                <Route
-                    path="/chapter/:statename"
-                    exact
-                    component={withRouter(Chapter)}
-                />
-                <Route path="/liveinoz" exact component={Liveinoz} />
-                <Route path="/liveinoz/:id" exact component={ReadMore} />
-                <Route path="/blog" exact component={Blog} />
-                <Route path="/contact" exact component={Contact} />
-                <Route path="/opportunities/:page" exact component={Scholarship} />
-                {/* <Route <NoFound/> /> */}
-            </Switch>
-            <Footer />
-        </Router>
-    )
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route
+          path="/chapter/:statename"
+          exact
+          component={withRouter(Chapter)}
+        />
+        <Route path="/liveinoz" exact component={Liveinoz} />
+        <Route path="/liveinoz/:id" exact component={ReadMore} />
+        <Route path="/blog" exact component={Blog} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/opportunities/:page" exact component={Scholarship} />
+        <Route path="*" exact={true} component={NotFound404} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
 
 // create opportunities function
-    // page 1 scholarships page
-        // Main Page
-        // Government
-        // University
-        // Testimony
+// page 1 scholarships page
+// Main Page
+// Government
+// University
+// Testimony
 
-    // page 2 Careers page
-        // Main Page
-            // Filter Function
-        // Job Postings
-            // Job Unavailable
+// page 2 Careers page
+// Main Page
+// Filter Function
+// Job Postings
+// Job Unavailable
---
+++ projectelementalreactapp/src/pages/404/PageUnavailable.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PageUnavailable.module.css';
+import Button from '../../components/Button';
+
+function PageUnavailable() {
+  return (
+    <div>
+      <h1>404 not Found</h1>
+      <Button>Return to Homepage</Button>
+    </div>
+  );
+}
+
+export default PageUnavailable;
