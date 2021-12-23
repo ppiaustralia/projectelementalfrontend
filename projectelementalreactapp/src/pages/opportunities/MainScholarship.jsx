f0a5a3049464874c1378ab7801b5a1f4418ea75c
Ricky Felix <rickyfelix19@outlook.com>
2021-12-23 15:35:54 +1100

    1 out of 3 layout done

--- projectelementalreactapp/src/pages/opportunities/MainScholarship.jsx
+++ projectelementalreactapp/src/pages/opportunities/MainScholarship.jsx
@@ -3,12 +3,25 @@ import React from 'react';
 import { UniversityUniversity as University } from './UniversityScholarship';
 import { GovernmentScholarship as Government } from './GovernmentScholarship';
 
-function MainScholarship() {
+import Button from '../../components/Button';
+export default function MainScholarship() {
   return (
     <div>
-      <p>HelloWorld</p>
+      {/* 1st layout -> 3 boxes */}
+      <div></div>
+
+      {/* 2nd layout -> testimonies  */}
+      <div></div>
+
+      {/* 3rd layout -> submit interest */}
+      <div>
+        <h3>Are you in a scholarship programme?</h3>
+        <p>
+          Do you want to share your experience and help others? Submit your
+          scholarship testimony by clicking the link below
+        </p>
+        <Button>Submit Scholarship</Button>
+      </div>
     </div>
   );
 }
-
-export default MainScholarship;
