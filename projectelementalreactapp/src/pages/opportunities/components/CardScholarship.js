a6f781e28adfc67a3496436bd784b563a006740f
Ricky Felix <rickyfelix19@outlook.com>
2021-12-23 15:36:09 +1100

    1 out of 3 layout done

---
+++ projectelementalreactapp/src/pages/opportunities/components/CardScholarship.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CardScholarship() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default CardScholarship
+
+// this card component is created for Scholarship
