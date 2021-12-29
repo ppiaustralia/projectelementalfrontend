8e91934382f969b88e915ea892455035bb2b561e
Ricky Felix <rickyfelix19@outlook.com>
2021-12-29 16:28:58 +1100

    change scholarship name

---
+++ projectelementalreactapp/src/pages/opportunities/Scholarship.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import FullScholarship from './pages/FullScholarship';
+import PartialScholarship from './pages/PartialScholarship';
+import ExchangeProgram from './pages/ExchangeProgram';
+import Testimony from './pages/Testimony';
+import { CardScholarship as Cards } from './components/CardScholarship';
+
+import Button from '../../components/Button';
+
+export default function Scholarship() {
+  return (
+    <div>
+      {/* 1st layout -> 3 boxes */}
+      <div>
+        <h4>
+          Are you an Indonesian who want to pursue higher education or join
+          exchange programs in Australia with a scholarship? Or an University
+          with Full or Partial Scholarship?
+        </h4>
+        <p>Check out our tips below</p>
+      </div>
+
+      {/* 2nd layout -> testimonies  */}
+      <Testimony />
+
+      {/* 3rd layout -> submit interest */}
+      <div>
+        <h3>Are you in a scholarship programme?</h3>
+        <p>
+          Do you want to share your experience and help others? Submit your
+          scholarship testimony by clicking the link below
+        </p>
+        <Button /> {/* link: https://forms.gle/g6r35XqLNfKz7Apr5 */}
+      </div>
+    </div>
+  );
+}
+
+/*
+  Scholarship
+    - Form:
+    https://docs.google.com/document/d/1xZtlqCVR5s_Scwz4NsltoAptu8xvPlv70jlnme-p1ZY/edit#
+
+  Testimony:
+    - Form:
+    https://docs.google.com/forms/u/1/d/1fiWdGO0s8fsQuaRHlL8QkPex07fw8XjnOf_TZHbDqsQ/edit?usp=drive_web
+    
+    - Photo:
+    https://drive.google.com/drive/u/1/folders/1pjB9JhLJ7dOdLKU7h_XQMzXyR6fIzshFQTI1H3zpE5Mx61dSRo4t9VjauTm6fJaqDptVySk4
+  */
