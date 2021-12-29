37a8316de4ebaf2addc98312ee7d9237ce3c4341
Ricky Felix <rickyfelix19@outlook.com>
2021-12-29 16:30:49 +1100

    add comments

--- projectelementalreactapp/src/pages/opportunities/components/TestimonyPreview.js
+++ projectelementalreactapp/src/pages/opportunities/components/TestimonyPreview.js
@@ -4,8 +4,11 @@ import 'TestimonyPreview.module.css';
 function TestimonyPreview(props) {
   return (
     <div>
-      <h1>TESTIMONIES</h1>
-      <p>Check out how some of our associates got their scholarship</p>
+      <div>
+        <h1>TESTIMONIES</h1>
+        <p>Check out how some of our associates got their scholarship</p>
+      </div>
+
       <div>
         {/* Carousel */}
         {props.image}
--- projectelementalreactapp/src/pages/opportunities/pages/ExchangeProgram.jsx
+++ projectelementalreactapp/src/pages/opportunities/pages/ExchangeProgram.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function ExchangeProgram() {
   return (
     <div>
+      {/* mapping */}
       <h1>Hello World</h1>
     </div>
   );
