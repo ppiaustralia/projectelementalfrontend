464fdc374d47002aa8e535bfa8bbdb5f8a529300
Erick Hartawan <hartawanerick@gmail.com>
2022-01-16 02:02:00 +1100

    updated react router

--- projectelementalreactapp/src/App.js
+++ projectelementalreactapp/src/App.js
@@ -1,6 +1,5 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { withRouter } from "react-router"
+import React, { useEffect } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "../src/pages/home/Home"
 import Navbar from "./layout/navbar/Navbar"
 
@@ -11,24 +10,40 @@ import Liveinoz, { ReadMore } from "../src/pages/liveinoz/Liveinoz"
 import Blog from "../src/pages/blog/Blog"
 // import { FooterContainer } from "./layout/footer/footer";
 import Footer from "./layout/newFooter/Footer"
+import { useSelector, useDispatch } from "react-redux"
+import {
+    setNews,
+    setLoadingTrue,
+    setLoadingFalse,
+} from "./store/news/newsSlice"
+import axios from "axios"
 
 function App() {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(setLoadingTrue())
+        axios
+            .get(`https://ppia-backend.herokuapp.com/feed/articles/`)
+            .then((data) => {
+                console.log(data.data)
+                dispatch(setNews(data.data))
+                dispatch(setLoadingFalse())
+            })
+    }, [])
+    const news = useSelector((state) => state.news.news)
+    const loading = useSelector((state) => state.news.loading)
     return (
         <Router>
             <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" exact component={About} />
-                <Route
-                    path="/chapter/:statename"
-                    exact
-                    component={withRouter(Chapter)}
-                />
-                <Route path="/liveinoz" exact component={Liveinoz} />
-                <Route path="/liveinoz/:id" exact component={ReadMore} />
-                <Route path="/blog" exact component={Blog} />
-                <Route path="/contact" exact component={Contact} />
-            </Switch>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="chapter/:statename" element={<Chapter />} />
+                <Route path="liveinoz" element={<Liveinoz />} />
+                <Route path="liveinoz/:id" element={<ReadMore />} />
+                <Route path="blog" element={<Blog />} />
+                <Route path="contact" element={<Contact />} />
+            </Routes>
             <Footer />
         </Router>
     )
--- projectelementalreactapp/src/pages/home/Home.jsx
+++ projectelementalreactapp/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react"
-
 import "./Home.css"
 import ImageSlider from "./ImageSlider/ImageSlider"
 import ChaptersGallery from "./ChaptersGallery/ChaptersGallery"
 import NewsCards from "./NewsCards/NewsCards"
+import { useSelector, useDispatch } from "react-redux"
 
 export default function Home() {
+    const news = useSelector((state) => state.news.news)
     return (
         <>
             <ImageSlider />
