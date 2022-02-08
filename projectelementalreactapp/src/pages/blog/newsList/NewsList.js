5b0498a075795045e4b88ffd3e92bab9b469405b
richardmwibowo <63839269+RichardWibowo@users.noreply.github.com>
2022-02-08 10:07:19 +0700

    Revert "Merge remote-tracking branch 'origin/erick-blog' into New-Footer-richard"
    This reverts commit bbaf17c7bc0e249d4fc3ccf66f21d2734b2f2037, reversing
    changes made to 86df73adac86763f30dd142e404c235f44200fbc.

--- projectelementalreactapp/src/pages/blog/newsList/NewsList.js
+++ projectelementalreactapp/src/pages/blog/newsList/NewsList.js
@@ -1,31 +1,13 @@
-import React, { useState } from "react"
+import React from "react"
 import NewsItem from "../newsItem/NewsItem"
-import Counter from "./counter/Counter"
 import s from "./NewsList.module.css"
 function NewsList(props) {
-    const handleIdxChange = (idx) => {
-        setCurrIdx(idx)
-    }
-
-    const { news, chunkSize } = props
-    const [currIdx, setCurrIdx] = useState(0)
-    let newsArray = []
-    for (let i = 0; i < news.length; i += chunkSize) {
-        let pageNewsArr = news.slice(i, i + chunkSize)
-        newsArray.push(pageNewsArr)
-    }
+    const { news } = props
     return (
         <div className={s.newsListCont}>
-            {newsArray[currIdx].map((eachNews) => (
+            {news.map((eachNews) => (
                 <NewsItem newsContent={eachNews} />
             ))}
-            <div className={s.counter}>
-                <Counter
-                    currIdx={currIdx}
-                    length={newsArray.length}
-                    handleIdxChange={handleIdxChange}
-                />
-            </div>
         </div>
     )
 }
