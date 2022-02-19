675d40499c7808cc480aeca7514db7fd6a83b672
Ricky Felix <rickyfelix19@outlook.com>
2022-02-19 17:20:52 +1100

    improve Home Overall Usability

--- projectelementalreactapp/src/pages/home/ChaptersGallery/ChaptersGallery.js
+++ projectelementalreactapp/src/pages/home/ChaptersGallery/ChaptersGallery.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import styles from "./ChaptersGallery.module.css"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './ChaptersGallery.module.css';
 
-import { Container, Row, Col, Image } from "react-bootstrap/"
-import { useSelector } from "react-redux"
+import { Container, Row, Col, Image } from 'react-bootstrap/';
+import { useSelector } from 'react-redux';
 
 export default function ChaptersGallery() {
-    const chapterBaseLink = "https://chapterslogo.s3.us-east-2.amazonaws.com/"
-    const chapters = useSelector((state) => state.chapters.chapters)
-    const branches = chapters.filter((eachData) => eachData.level === 1)
-    return (
-        <div className={styles["front-page"]}>
-            <div className={styles.chaptersTitle}>
-                <p>Chapters</p>
-            </div>
+  const chapterBaseLink = 'https://chapterslogo.s3.us-east-2.amazonaws.com/';
+  const chapters = useSelector((state) => state.chapters.chapters);
+  const branches = chapters.filter((eachData) => eachData.level === 1);
+  return (
+    <div className={styles['front-page']}>
+      <div className={styles.chaptersTitle}>
+        <p>Chapters</p>
+      </div>
 
-            <div className={styles["img-gallery"]}>
-                <Container>
-                    <Row className="justify-content-md-center">
-                        {branches.map((branch, i) => {
-                            return (
-                                <Col md="auto">
-                                    <Link to={`chapter/${branch.state}`}>
-                                        <Image
-                                            src={`${chapterBaseLink}${branch.image}`}
-                                            className={`d-block ${styles["responsive-gallery"]} img-fluid mx-4`}
-                                            // roundedCircle
-                                            style={{ objectFit: "contain" }}
-                                        />
-                                    </Link>
-                                    <h5>{branch.title}</h5>
-                                </Col>
-                            )
-                        })}
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    )
+      <div className={styles['img-gallery']}>
+        <Container>
+          <Row className="justify-content-md-center">
+            {branches.map((branch, i) => {
+              return (
+                <Col md="auto">
+                  <Link to={`chapter/${branch.state}`}>
+                    <Image
+                      src={`${chapterBaseLink}${branch.image}`}
+                      className={`d-block ${styles['responsive-gallery']} mx-5`}
+                      // roundedCircle
+                      style={{ objectFit: 'contain' }}
+                    />
+                    <h5 className={styles.chaptersName}>{branch.name}</h5>
+                  </Link>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
