9d22a676b304ae8285aad412cba8c1808e23ad5d
Ricky Felix <rickyfelix19@outlook.com>
2022-03-05 21:50:17 +1100

    create 404 styling

--- projectelementalreactapp/src/pages/404/PageUnavailable.jsx
+++ projectelementalreactapp/src/pages/404/PageUnavailable.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import './PageUnavailable.module.css';
-import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
+import style from './PageUnavailable.module.css';
+// import Button from '../../components/Button';
 
 function PageUnavailable() {
   console.log('test');
 
   return (
-    <div>
-      <h1>404 not Found</h1>
-      <h5>The link you are looking for cannot be found.</h5>
-    </div>
+    <>
+      <div className={style.center}>
+        <h1 className={style.seperator}>404 not Found</h1>
+        <p>The link you are looking for cannot be found.</p>
+        <Link to="/">
+          <button>Return to Home</button>
+        </Link>
+      </div>
+    </>
   );
 }
 
