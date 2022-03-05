3810d156b18de484bbcae6a02b4e70c792029740
Erick Hartawan <hartawanerick@gmail.com>
2022-03-05 13:24:02 +1030

    re-instated missing files after merging

--- projectelementalreactapp/src/pages/blog/sidebar/recentPost/SmallNewsItem.js
+++ projectelementalreactapp/src/pages/blog/sidebar/recentPost/SmallNewsItem.js
@@ -1,15 +1,14 @@
 import React from "react"
+
 import s from "./SmallNewsItem.module.css"
 
-function SmallNewsItem() {
+function SmallNewsItem(props) {
+    const { title, tag, content, publish_date, image, slug, is_deleted } =
+        props.newsContent
     return (
         <div className={s.smallNewsCont}>
-            <img
-                src="https://uc.uxpin.com/files/1165148/1125601/PPIA_1605140707463-bc3ec2c4b4f96c634f9cb3dbea232400-0115a1.jpg"
-                alt="put title here"
-                className={s.thumbnail}
-            />
-            <div className={s.newsTitle}>Some sample title of PPIA HAHA</div>
+            <img src={`${image}`} alt={`${slug}`} className={s.thumbnail} />
+            <div className={s.newsTitle}>{title}</div>
         </div>
     )
 }
