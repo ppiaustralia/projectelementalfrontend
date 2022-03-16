75693cb90df16a4615f1cccfc85fafdc9f9e20c9
Ricky Felix <rickyfelix19@outlook.com>
2022-03-17 00:02:05 +1100

    add cat easter eggs

--- projectelementalreactapp/src/pages/404/PageUnavailable.js
+++ projectelementalreactapp/src/pages/404/PageUnavailable.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import style from './PageUnavailable.module.css';
 // import Button from '../../components/Button';
 
+import Cat from './cat.jpg';
+
 function PageUnavailable() {
   console.log('test');
 
@@ -10,8 +12,11 @@ function PageUnavailable() {
     <>
       <div className={style.center}>
         <h1 className={style.seperator}>404 not Found</h1>
-        <p>The link you are looking for cannot be found.</p>
-        <Link to="">
+        <p>
+          The link you are looking for cannot be found. Here's a cat <br />
+          <img src={Cat} alt="cat-image" />
+        </p>
+        <Link to="/">
           <button>Return to Home</button>
         </Link>
       </div>
