a089134276e08a2f36c86dece71cd356fb398a46
Ricky Felix <rickyfelix19@outlook.com>
2022-03-16 23:57:59 +1100

    install react-responsive-carousel

--- projectelementalreactapp/src/pages/opportunities/scholarship/components/Testimony.js
+++ projectelementalreactapp/src/pages/opportunities/scholarship/components/Testimony.js
@@ -3,45 +3,44 @@ import TestimonyDB from '../database/Testimony.json';
 
 // import TestimonyPreview from './components/TestimonyPreview';
 import styles from './Testimony.module.css';
+import ReactDOM from 'react-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import cat from './cat.jpg';
 
 // import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.css';
 
 function Testimony() {
   return (
     <div className={styles.testimony}>
-      <h2>
-        <b>Testimonies</b>
-      </h2>
-      <p>
-        Check out how some of our associates
-        <br />
-        got their scholarship
-      </p>
-      <div className={styles.testimony__carousel}>
+      <div className={styles.descripiton}>
+        <h2>
+          <b>Testimonies</b>
+        </h2>
+        <p>
+          Check out how some of our associates
+          <br />
+          got their scholarship
+        </p>
+      </div>
+
+      <div className={styles.testimonyCarousel}>
         <Carousel>
-          <Carousel.Item interval={1500}>
-            <img
-              src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-              alt="Image One"
-            />
-            <Carousel.Caption>
-              <h3>Label for first slide</h3>
-              <p>Sample Text for Image One</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={1500}>
-            <img
-              src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-              alt="Image Two"
-            />
-            <Carousel.Caption>
-              <h3>Label for second slide</h3>
-              <p>Sample Text for Image Two</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          <div>
+            <img src={cat} />
+            <p className="legend">Cat 1</p>
+          </div>
+          <div>
+            <img src={cat} />
+            <p className="legend">Cat 2</p>
+          </div>
+          <div>
+            <img src={cat} />
+            <p className="legend">Cat 3</p>
+          </div>
         </Carousel>
+        {/* https://www.npmjs.com/package/react-responsive-carousel */}
       </div>
     </div>
   );
