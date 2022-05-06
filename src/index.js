23b76813c5c5c26d8bb770c589c0b59fc440db6a
Erick Hartawan <hartawanerick@gmail.com>
2022-05-06 13:03:13 +0930

    fixed navbar responsive on mobile

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css'; // normalize
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GlobalStyles } from './global-styles';
-import store from './store/store';
-import { Provider } from 'react-redux';
+import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react"
+import ReactDOM from "react-dom"
+import "normalize.css" // normalize
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { GlobalStyles } from "./global-styles"
+import store from "./store/store"
+import { Provider } from "react-redux"
 
 ReactDOM.render(
-  <>
-    <Provider store={store}>
-      <GlobalStyles />
-      <App />
-    </Provider>
-  </>,
-  document.getElementById('root')
-);
+    <>
+        <Provider store={store}>
+            <GlobalStyles />
+            <App />
+        </Provider>
+    </>,
+    document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -1,130 +1,135 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css';
-import DropdownChapter from './DropdownChapter';
-import DropdownOpportunities from './DropdownOpportunities';
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import styles from "./Navbar.module.css"
+import DropdownChapter from "./DropdownChapter"
+import DropdownOpportunities from "./DropdownOpportunities"
 
 function Navbar() {
-  const [click, setclick] = useState(false);
-  const [opportunitiesDropdown, setDropdownOpportunities] = useState(false);
-  const [chapterDropdown, setChapterDropdown] = useState(false);
+    const [click, setclick] = useState(false)
+    const [opportunitiesDropdown, setDropdownOpportunities] = useState(false)
+    const [chapterDropdown, setChapterDropdown] = useState(false)
 
-  const handleClick = () => setclick(!click);
-  const closeMobileMenu = () => setclick(false);
+    const handleClick = () => setclick(!click)
+    const closeMobileMenu = () => setclick(false)
 
-  const onMouseEnterOpportunities = () => {
-    if (window.innerWidth < 960) {
-      setDropdownOpportunities(false);
-    } else {
-      setDropdownOpportunities(true);
+    const onMouseEnterOpportunities = () => {
+        if (window.innerWidth < 960) {
+            setDropdownOpportunities(false)
+        } else {
+            setDropdownOpportunities(true)
+        }
     }
-  };
-  const onMouseEnterChapter = () => {
-    if (window.innerWidth < 960) {
-      setChapterDropdown(false);
-    } else {
-      setChapterDropdown(true);
+    const onMouseEnterChapter = () => {
+        if (window.innerWidth < 960) {
+            setChapterDropdown(false)
+        } else {
+            setChapterDropdown(true)
+        }
     }
-  };
-  const onMouseLeaveChapter = () => {
-    if (window.innerWidth < 960) {
-      setChapterDropdown(false);
-    } else {
-      setChapterDropdown(false);
+    const onMouseLeaveChapter = () => {
+        if (window.innerWidth < 960) {
+            setChapterDropdown(false)
+        } else {
+            setChapterDropdown(false)
+        }
     }
-  };
 
-  const onMouseLeaveOpportunities = () => {
-    if (window.innerWidth < 960) {
-      setDropdownOpportunities(false);
-    } else {
-      setDropdownOpportunities(false);
+    const onMouseLeaveOpportunities = () => {
+        if (window.innerWidth < 960) {
+            setDropdownOpportunities(false)
+        } else {
+            setDropdownOpportunities(false)
+        }
     }
-  };
 
-  return (
-    <>
-      <nav className={styles.navbar}>
-        <Link to="/alpha/" className={styles['navbar-logo']}>
-          <img
-            src={`https://chapterslogo.s3.us-east-2.amazonaws.com/PPIA.png`}
-            alt="PPIA logo"
-            className={styles.ppiaLogo}
-          />
-        </Link>
-        <div className={styles['menu-icon']} onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
-        <ul
-          className={
-            click
-              ? `${styles['nav-menu']} ${styles.active}`
-              : `${styles['nav-menu']}`
-          }
-        >
-          <li className={styles['nav-item']}>
-            <Link
-              to="/alpha"
-              className={styles['nav-links']}
-              onClick={closeMobileMenu}
-            >
-              Home
-            </Link>
-          </li>
-          <li className={styles['nav-item']}>
-            <Link
-              to="/alpha/about"
-              className={styles['nav-links']}
-              onClick={closeMobileMenu}
-            >
-              About Us
-            </Link>
-          </li>
-          <li
-            className={styles['nav-item']}
-            onMouseEnter={onMouseEnterChapter}
-            onMouseLeave={onMouseLeaveChapter}
-          >
-            {/* changed from "Link" to "p" to prevent redirect to /chapter/ */}
-            <div className={styles['nav-links']} onClick={closeMobileMenu}>
-              <Link
-                to="/alpha/chapter/all"
-                className={styles['nav-links']}
-                onClick={closeMobileMenu}
-              >
-                Chapters <i className="fas fa-caret-down" />
-              </Link>
-              {chapterDropdown && <DropdownChapter />}
-            </div>
-          </li>
-          <li className={styles['nav-item']}>
-            <Link
-              to="/alpha/liveinoz"
-              className={styles['nav-links']}
-              onClick={closeMobileMenu}
-            >
-              Live in OZ
-            </Link>
-          </li>
-          <li className={styles['nav-item']}>
-            <Link
-              to="/alpha/blog"
-              className={styles['nav-links']}
-              onClick={closeMobileMenu}
-            >
-              Blog
-            </Link>
-          </li>
-          <li className={styles['nav-item']}>
-            <Link
-              to="/alpha/contact"
-              className={styles['nav-links']}
-              onClick={closeMobileMenu}
-            >
-              Contact
-            </Link>
-          </li>
-          {/* <li
+    return (
+        <>
+            <nav className={styles.navbar}>
+                <div>
+                    <Link to="/alpha/" className={styles["navbar-logo"]}>
+                        <img
+                            src={`https://chapterslogo.s3.us-east-2.amazonaws.com/PPIA-WHITE.png`}
+                            alt="PPIA logo"
+                            className={styles.ppiaLogo}
+                        />
+                    </Link>
+                </div>
+                <div className={styles["menu-icon"]} onClick={handleClick}>
+                    <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                </div>
+                <ul
+                    className={
+                        click
+                            ? `${styles["nav-menu"]} ${styles.active}`
+                            : `${styles["nav-menu"]}`
+                    }
+                >
+                    <li className={styles["nav-item"]}>
+                        <Link
+                            to="/alpha"
+                            className={styles["nav-links"]}
+                            onClick={closeMobileMenu}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li className={styles["nav-item"]}>
+                        <Link
+                            to="/alpha/about"
+                            className={styles["nav-links"]}
+                            onClick={closeMobileMenu}
+                        >
+                            About Us
+                        </Link>
+                    </li>
+                    <li
+                        className={styles["nav-item"]}
+                        onMouseEnter={onMouseEnterChapter}
+                        onMouseLeave={onMouseLeaveChapter}
+                    >
+                        {/* changed from "Link" to "p" to prevent redirect to /chapter/ */}
+                        <div
+                            className={styles["nav-links"]}
+                            onClick={closeMobileMenu}
+                        >
+                            <Link
+                                to="/alpha/chapter/all"
+                                className={styles["nav-links"]}
+                                onClick={closeMobileMenu}
+                            >
+                                Chapters <i className="fas fa-caret-down" />
+                            </Link>
+                            {chapterDropdown && <DropdownChapter />}
+                        </div>
+                    </li>
+                    <li className={styles["nav-item"]}>
+                        <Link
+                            to="/alpha/liveinoz"
+                            className={styles["nav-links"]}
+                            onClick={closeMobileMenu}
+                        >
+                            Live in OZ
+                        </Link>
+                    </li>
+                    <li className={styles["nav-item"]}>
+                        <Link
+                            to="/alpha/blog"
+                            className={styles["nav-links"]}
+                            onClick={closeMobileMenu}
+                        >
+                            Blog
+                        </Link>
+                    </li>
+                    <li className={styles["nav-item"]}>
+                        <Link
+                            to="/alpha/contact"
+                            className={styles["nav-links"]}
+                            onClick={closeMobileMenu}
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                    {/* <li
             className={styles['nav-item']}
             onMouseEnter={onMouseEnterOpportunities}
             onMouseLeave={onMouseLeaveOpportunities}
@@ -140,13 +145,13 @@ function Navbar() {
               {opportunitiesDropdown && <DropdownOpportunities />}
             </div>
           </li> */}
-        </ul>
-      </nav>
-    </>
-  );
+                </ul>
+            </nav>
+        </>
+    )
 }
 
-export default Navbar;
+export default Navbar
 
 //className="(\w*-\w*)"
 //className={styles['$1']}
