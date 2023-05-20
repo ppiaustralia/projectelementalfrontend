f8bf3b83ca2a6a0fd6b106906bf682e1dba153ae
eaglethrost <dimazanugrah12@gmail.com>
2023-05-20 19:42:56 +0930

    fix & responsive imgSlider

--- src/pages/home/ImageSlider/ImageSlider.js
+++ src/pages/home/ImageSlider/ImageSlider.js
@@ -9,9 +9,10 @@ import ImageHeader from '../../../assets/images/2.jpg';
 export default function ImageSlider() {
   const slideshow = useSelector((state) => state.slideshow.slideshow);
   return (
+    // <div className="flex min-h-[50vh] relative z-[5] overflow-hidden p-[2em]">
     <div className={styles.container}>
       <span className={styles.backgroundImage} />
-      <div className=" flex flex-col text-lg basis-1/3 p-2 px-4  items-center">
+      <div className="flex flex-col text-lg p-2 px-4 items-center">
         <h2 className="text-5xl text-red-600 mb-2">PPI-Australia (PPIA)</h2>
         <p className={styles.welcomeSubtitle}>
           Perhimpunan Pelajar Indonesia Australia (PPIA) or Indonesian Students'
@@ -22,7 +23,6 @@ export default function ImageSlider() {
           <Link to="about/" className={styles.findOut}>
             <span>Learn More</span>
           </Link>
-
           <a
             href="https://www.ppi-australia.org/kipi-2022/"
             className={styles.contactUs}
@@ -31,7 +31,7 @@ export default function ImageSlider() {
           </a>
         </div>
       </div>
-      <div className='basis-2/3'>
+      <div className=''>
         <img src={ImageHeader} alt="Synergy for Indonesia image" className='w-full h-full object-contain' />
       </div>
 
